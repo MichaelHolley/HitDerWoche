dc2a0d2362249e00e833ccc36640b039744dd3b7
Michael Holley <michael.philipp.holley@gmail.com>
2023-07-06 22:49:47 +0200

    Spotify Preview

---
+++ webapp/src/views/PreviewView.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <div class="hero min-h-screen">
+      <div class="hero-content text-center w-full md:w-3/4 flex flex-col">
+        <RouterLink to="/" class="btn btn-outline btn-primary">Zurück zur Home-Ansicht</RouterLink>
+        <iframe
+          style="border-radius: 12px"
+          src="https://open.spotify.com/embed/playlist/4RnajpiFvsWCxU3xqwXXrc?utm_source=generator&theme=0"
+          width="100%"
+          height="600"
+          frameBorder="0"
+          allowfullscreen="true"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+          class="my-4"
+        ></iframe>
+      </div>
+    </div>
+  </main>
+</template>
