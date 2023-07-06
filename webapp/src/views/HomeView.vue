20817b70e0b90964c9f5e83c746889f7421e8962
Michael Holley <michael.philipp.holley@gmail.com>
2023-07-06 19:04:42 +0200

    added daisyui and cleaned up

--- webapp/src/views/HomeView.vue
+++ webapp/src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <div class="h-1">Hit der Woche</div>
   </main>
 </template>
--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
+const daisyui = require('daisyui')
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['forest']
+  }
 }
