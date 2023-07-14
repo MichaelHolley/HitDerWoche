afc28476f0f99f803988b53fdd771fc3a842aeb2
Michael Holley <michael.philipp.holley@gmail.com>
2023-07-14 17:03:54 +0200

    cronjob spotify implementation

--- Backend/cronjob/main.go
+++ Backend/cronjob/main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"encoding/json"
+  "github.com/robfig/cron"
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
+	"time"
 )
+
+// URL of the authentication endpoint
+const authURL = "https://accounts.spotify.com/api/token"
+
+// Client credentials
+const clientID = ""
+const clientSecret = ""
+
+// Playlist ID
+const playlistID = ""
 
 type PlaylistResponse struct {
 	Items []struct {
@@ -15,18 +29,72 @@ type PlaylistResponse struct {
 	} `json:"items"`
 }
 
-func main() {
-	// Set your Spotify access token here
-	accessToken := "<your-access-token>"
+type AuthResponse struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int    `json:"expires_in"`
+	TokenType   string `json:"token_type"`
+}
 
-	// Set the Spotify playlist ID here
-	playlistID := "4RnajpiFvsWCxU3xqwXXrc"
+func requestAccessToken() (token string, success bool) {
+
+	// Create the form data with client credentials
+	formData := url.Values{}
+	formData.Set("grant_type", "client_credentials")
+	formData.Set("client_id", clientID)
+	formData.Set("client_secret", clientSecret)
+
+	// Prepare the request
+	req, err := http.NewRequest("POST", authURL, strings.NewReader(formData.Encode()))
+	if err != nil {
+		fmt.Println("Failed to create request:", err)
+		return "", false
+	}
+
+	// Set the request headers
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	// Create a new HTTP client
+	client := &http.Client{}
+
+	// Send the request
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Failed to send request:", err)
+		return "", false
+	}
+	defer resp.Body.Close()
+
+	// Check the response status code
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Authentication failed. Status code:", resp.StatusCode)
+		return "", false
+	}
+
+	var authResponse AuthResponse
+	err = json.NewDecoder(resp.Body).Decode(&authResponse)
+	if err != nil {
+		fmt.Println("Failed to parse JSON response:", err)
+		return
+	}
+
+	// Access the parsed response fields
+	fmt.Println("Access Token:", authResponse.AccessToken)
+
+	return authResponse.AccessToken, true
+}
+
+func updateData() {
+	// Set your Spotify access token here
+	accessToken, success := requestAccessToken()
+	if success != true {
+		return
+	}
 
 	// Create an HTTP client
 	client := &http.Client{}
 
 	// Create a GET request to retrieve the playlist
-	url := fmt.Sprintf("https://api.spotify.com/v1/playlists/{playlist_id}", playlistID)
+	url := fmt.Sprintf("https://api.spotify.com/v1/playlists/%s/tracks", playlistID)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -61,4 +129,20 @@ func main() {
 	}
 
 	// TODO: Write to DB
+}
+
+func main() {
+	// Create a new cron scheduler
+	c := cron.New()
+
+	// Schedule the cron job to run every 15 minutes
+	c.AddFunc("*/15 * * * *", func() {
+			fmt.Println("Running cronjob at:", time.Now())
+			updateData()
+	})
+
+	// Start the cron scheduler
+	c.Start()
+
+	select {}
 }
