69746ba2623e829dc070d3bff75ad816c5f11823
Michael Holley <michael.philipp.holley@gmail.com>
2023-07-29 01:18:59 +0200

    CORS

--- backend/main.go
+++ backend/main.go
@@ -29,6 +29,8 @@ func startCronJob() {
 func startApi() {
 	router := gin.Default()
 
+	router.Use(corsMiddleware())
+
 	router.GET("/tracks", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"items": services.GetTracks()})
 	})
@@ -46,4 +48,21 @@ func main() {
 	go startApi()
 
 	select {}
+}
+
+// Define the Cors middleware to allow all CORS requests
+func corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Authorization, Content-Type")
+
+		// Handle preflight requests
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+			return
+		}
+
+		c.Next()
+	}
 }
