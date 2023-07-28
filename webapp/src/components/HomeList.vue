c341705333340c0e2e87a24c39246e6cb80521e8
Michael Holley <michael.philipp.holley@gmail.com>
2023-07-29 01:18:47 +0200

    frontend changes

--- webapp/src/components/HomeList.vue
+++ webapp/src/components/HomeList.vue
@@ -1,36 +1,17 @@
 <script setup lang="ts">
   import { reactive } from 'vue'
+  import { type Track } from '../types'
+  import { formatDuration } from '../utils/datetime'
 
-  const data = reactive([
-    {
-      id: '123',
-      title: 'the very cool song title',
-      artist: 'the artist',
-      releasYear: 1999,
-      image: 'https://cataas.com/cat/says/hello%20world!'
-    },
-    {
-      id: '123',
-      title: 'the very cool song title',
-      artist: 'the artist',
-      releasYear: 1999,
-      image: 'https://cataas.com/cat/says/hello%20world!'
-    },
-    {
-      id: '123',
-      title: 'the very cool song title',
-      artist: 'the artist',
-      releasYear: 1999,
-      image: 'https://cataas.com/cat/says/hello%20world!'
-    },
-    {
-      id: '123',
-      title: 'the very cool song title',
-      artist: 'the artist',
-      releasYear: 1999,
-      image: 'https://cataas.com/cat/says/hello%20world!'
-    }
-  ])
+  let tracks: Track[] = reactive([])
+
+  fetch('http://localhost:5000/tracks')
+    .then((resp) => resp.json())
+    .then((data) => {
+      tracks.push(
+        ...(data.items as Track[]).sort((t1, t2) => t1.playlistPosition - t2.playlistPosition)
+      )
+    })
 </script>
 
 <template>
@@ -39,21 +20,21 @@
     <div class="xl:w-8/12 md:w-10/12 sm:w-11/12 w-full px-3 sm:px-0 mx-auto">
       <ul>
         <li
-          v-for="song in data"
-          :key="song.id"
-          class="my-4 rounded-2xl py-3 px-6 border-2 border-stone-700 bg-stone-900 hover:bg-stone-800 shadow-sm shadow-emerald-600 hover:cursor-pointer"
+          v-for="track in tracks"
+          :key="track.id"
+          class="my-4 rounded-2xl py-3 px-6 border-2 border-stone-700 bg-stone-900 hover:bg-stone-800 shadow-sm shadow-green-400 hover:cursor-pointer"
         >
           <div class="flex flex-row">
-            <img :src="song.image" loading="lazy" class="h-28 image-full rounded mr-6" />
+            <!-- <img :src="track.image" loading="lazy" class="h-28 image-full rounded mr-6" /> -->
             <div>
               <div class="text-2xl font-medium">
-                {{ song.title }}
+                {{ track.name }}
               </div>
               <div class="text-lg">
-                {{ song.artist }}
+                {{ track.artists }}
               </div>
               <div>
-                {{ song.releasYear }}
+                {{ formatDuration(track.duration_ms) }}
               </div>
             </div>
           </div>
---
+++ webapp/src/types.ts
@@ -0,0 +1,14 @@
+export interface Track {
+  id: string
+  playlistPosition: number
+  artists: string
+  disc_number: number
+  duration_ms: number
+  explicit: boolean
+  href: string
+  name: string
+  preview_url: string
+  track_number: number
+  uri: string
+  type: string
+}
