cfd441bcff08ed673f6bcd1bf586429c434b8229
Michael Holley <michael.philipp.holley@gmail.com>
2023-08-06 11:59:33 +0200

    if else for undefined track display

--- webapp/src/components/AudioPlayer/TrackDisplay.vue
+++ webapp/src/components/AudioPlayer/TrackDisplay.vue
@@ -5,11 +5,12 @@
 </script>
 
 <template>
-  <div class="flex flex-row justify-start items-center">
+  <span v-if="!currentTrack"></span>
+  <div v-else class="flex flex-row justify-start items-center">
     <p>
-      <span class="text-stone-600">{{ '#' + currentTrack?.playlist_position }}</span>
-      <span class="mx-2 font-bold">{{ currentTrack?.name }}</span>
-      <span class="text-stone-600">{{ currentTrack?.artists }}</span>
+      <span class="text-stone-600">{{ '#' + currentTrack.playlist_position }}</span>
+      <span class="mx-2 font-bold">{{ currentTrack.name }}</span>
+      <span class="text-stone-600">{{ currentTrack.artists }}</span>
     </p>
   </div>
 </template>
